e8d6d133263df46c27388c00469336aef3934df2
Cory Chambers <corytyme@yahoo.com>
2014-08-25 08:34:40 -0400

    solved the challenege

---
+++ UITableViewController Challenge Solution/CCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Cory Chambers on 8/25/14.
+//  Copyright (c) 2014 NA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTableViewController : UITableViewController
+
+@end
